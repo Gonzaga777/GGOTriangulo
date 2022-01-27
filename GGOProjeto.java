31d2f08c905fdebec05317349a77e2a1671ba7de
Gabriel <gabrieloliveiragonzaga1@hotmail.com>
2022-01-26 21:30:55 -0300

    Criação do Servlet e Logica do Quadrado Retangulo

---
+++ GGOProjeto.java
@@ -0,0 +1,77 @@
+package com.br.rd;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GGOProjeto
+ */
+@WebServlet("/GGOProjeto")
+public class GGOProjeto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GGOProjeto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		int val1 = Integer.parseInt(request.getParameter("val1"));
+		int val2 = Integer.parseInt(request.getParameter("val2"));
+		int val3 = Integer.parseInt(request.getParameter("val3"));
+		int result ;
+		
+		// calculando se o quadrado é retangulo ou não
+		if (val1 == 0 && val2 > 0 && val3 > 0 && (val2 == val3)){
+			result = val2 * val3;
+			response.getWriter().println("é um quadrado e a Area e igual a:" + result);
+		}else if(val2 == 0 && val3 > 0 && val1 > 0 && (val1 == val3)) {
+			result = val3 * val1;
+			response.getWriter().println("é um quadrado  e a Area e igual a:" + result);
+		}else if(val3 == 0 && val1 > 0 && val2 > 0 && (val2 == val1)) {
+			result = val2 * val1;
+			response.getWriter().println("é um quadrado  e a Area e igual a:" + result);
+		}
+		else if(val3 == 0 && val1 > 0 && val2 > 0 && val1 > 0 && ( val1 != val2)) {
+			result = val1 * val2;
+			response.getWriter().println("é um quadrado Retangulo e a Area e igual a:" + result);
+		}
+		else if(val2 == 0 && val3 > 0 && val1 > 0 &&  (val1 != val3)) {
+			result = val1 * val3;
+			response.getWriter().println("quadrado Retangulo e a Area e igual a:" + result);
+		}else if(val1 == 0 && val2 > 0 && val3 > 0 &&  (val2 != val3)) {
+			result = val2 * val3;
+			response.getWriter().println("quadrado Retangulo e a Area e igual a:" + result);
+		}
+		
+		
+
+		
+	
+	
+	
+		
+
+	}
+}
+
--- index.jsp
+++ index.jsp
@@ -7,25 +7,28 @@
 <title>Projeto Triangulo</title>
 </head>
 <body>
+
+<h2>Programa para calcular Quadrados , descobrir se é triangulo e o tipo , e se é uma Reta</h2>
+<h3>VALOR NULO É IGUAL A == 0 (CAMPOS OBRIGATÓRIOS)</h3>
 <form method="post" action="GGOProjeto">
 	<div class="mb-4 col-4">
 	Primeiro Valor
-	<input type="text" name="val1" class="form-control">
+	<input type="text" name="val1" class="form-control" required="required">
 	
 	</div>
 	<div class="mb-4 col-4">
 	Segundo Valor
-	<input type="text" name="val2" id="valor1" class="form-control">
+	<input type="text" name="val2" id="valor1" class="form-control" required="required">
 	</div>
 	
 	<div class="mb-4 col-4">
 	Terceiro Valor
-	<input type="text" name="val3" id="valor1" class="form-control">
+	<input type="text" name="val3" id="valor1" class="form-control"required="required" >
 	</div>
 	
 	<button type="reset" class="btn btn-secondary">Limpar</button>
-	<button type="submit" class="btn btn-secondary">Somar</button>
+	<button type="submit" class="btn btn-secondary">Calculo</button>
 	</form>
-	
+</div>	
 </body>
 </html>
